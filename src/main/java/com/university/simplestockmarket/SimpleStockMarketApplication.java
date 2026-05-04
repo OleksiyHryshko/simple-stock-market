319f0e42d02525d65bb1409e5664d7cdf03f36ea
OleksiiHryshko <oleksij.gryszko@gmail.com>
2026-05-04 16:47:07 +0200

    Initial Spring Boot setup

---
+++ src/main/java/com/university/simplestockmarket/SimpleStockMarketApplication.java
@@ -0,0 +1,13 @@
+package com.university.simplestockmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleStockMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleStockMarketApplication.class, args);
+    }
+
+}
